9d1eaf81d8c40ce62b707bbd20b88d7bf756bc4b
Leonie Spangenberg <leoniespangenberg@Mac.fritz.box>
2026-07-17 19:55:14 +0200

    Added China relevance filter and CSV export

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from newsapi import NewsApiClient
 from dotenv import load_dotenv
+from newsapi import NewsApiClient
 import os
+import pandas as pd
 
 load_dotenv(dotenv_path=".env")
 
@@ -12,27 +13,48 @@ articles = newsapi.get_everything(
     q="China",
     language="en",
     sort_by="publishedAt",
-    page_size=1
+    page_size=100
 )
 
-article = articles["articles"][0]
-print("\nTITLE:")
-print(article["title"])
+data = []
+for article in articles["articles"]:
+     
+    score = 0
 
-print("\nAUTHOR:")
-print(article["author"])
+    title = article["title"] or " "
+    content = article["content"] or " "
 
-print("\nSOURCE:")
-print(article["source"]["name"])
+    keywords = [
+        "China",
+        "Chinese",
+        "Xi",
+        "Beijing",
+        "Shanghai",
+        "Hong Kong"
+    ]
 
-print("\nDATE:")
-print(article["publishedAt"])
+    for keyword in keywords:
 
-print("\nDESCRIPTION:")
-print(article["description"])
+        if keyword in article["title"]:
+            score += 2
 
-print("\nURL:")
-print(article["url"])
+        if keyword in article["content"]:
+            score += 1
 
-print("\nCONTENT:")
-print(article["content"])
+    data.append({
+            "title": article["title"],
+            "author": article["author"],
+            "source": article["source"],
+            "publishedAt": article["publishedAt"],
+            "description": article["description"],
+            "url": article["url"],
+            "content": content,
+            "china_score": score
+        })
+
+df = pd.DataFrame(data)
+
+china_articles = df[df["china_score"] > 0]
+print(china_articles[["title", "china_score"]])
+
+china_articles.to_csv("china_articles.csv", index=False)
